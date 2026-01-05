57f0129e4fada5b9d4bc4cc9037d830d12448b90
Ali Safari <asafarim.it@gmail.com>
2026-01-05 14:06:48 +0100

    chore: update base URL configuration for GitHub Pages deployment

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/react-dropdowns/',
+    base: '/',
     plugins: [react()],
   resolve: {
     alias: {
